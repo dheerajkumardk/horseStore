a26e3631581661e4e87b75d59d6c68a1e5b373a5
dheerajkumardk <dyaduwanshi0@gmail.com>
2024-12-04 14:28:22 +0530

    add tests for sol and huff contract

---
+++ test/v1/Base_TestV1.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
+import {IHorseStore} from "../../src/horseStoreV1/IHorseStore.sol";
+import {Test, console2} from "forge-std/Test.sol";
+
+abstract contract Base_TestV1 is Test {
+    IHorseStore public horseStore;
+
+    function setUp() public virtual {
+        horseStore = IHorseStore(address(new HorseStore()));
+    }
+
+    function testReadValue() public {
+        uint256 initialValue = horseStore.readNumberOfHorses();
+        assertEq(initialValue, 0);
+    }
+
+    function testWriteValue(uint256 horseNumber) public {
+        horseStore.updateHorseNumber(horseNumber);
+        assertEq(horseStore.readNumberOfHorses(), horseNumber);
+    }
+}
---
+++ test/v1/HorseStoreHuff.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+import {Base_TestV1, IHorseStore} from "./Base_TestV1.t.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+
+contract HorseStoreHuff is Base_TestV1 {
+    string public constant HORSE_STORE_HUFF_LOCATION = "horseStoreV1/HorseStore";
+
+    function setUp() public override {
+        // HorseStore -> Huff edition
+        horseStore = IHorseStore(HuffDeployer.config().deploy(HORSE_STORE_HUFF_LOCATION));
+    }
+}
